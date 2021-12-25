554bcfcc6bb652bd2a2ff5c72d171e3b9dc406ec
vtf05 <05avinashvish@gmail.com>
2021-12-25 15:58:01 +0530

    inital commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,request, url_for, redirect, render_template
+import pickle
+import numpy as np
+import sklearn
+
+app = Flask(__name__,template_folder='templates')
+
+model=pickle.load(open('model.pkl1','rb'))
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    int_features=[int(x) for x in request.form.values()]
+    final=[np.array(int_features)]
+   
+   
+    prediction=model.predict(final)
+    output=round(prediction[0], 2)
+
+    return render_template('index.html',prediction_text='Your predicted percentage in 6th sem is.\nProbabily {}'.format(output))
+
+if __name__ == '__main__':
+    app.run(debug=True)
